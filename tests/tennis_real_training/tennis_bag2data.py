61ffdecc46d093d04ebc4e559996268101a7270d
Qingyu Xiao <xiaoqingyu0113@gmail.com>
2024-12-10 14:06:54 -0500

    add calibration Jul18

--- tests/tennis_real_training/tennis_bag2data.py
+++ tests/tennis_real_training/tennis_bag2data.py
@@ -1,8 +1,9 @@
 import numpy as np
 import rosbag
 import matplotlib.pyplot as plt
-
-
+from pycamera import triangulate, CameraParam, set_axes_equal
+import yaml
+import gtsam
 
 def msg_parser(msg):
     t = msg.header.stamp.to_sec()
@@ -38,9 +39,50 @@ def view_points():
         ax.invert_yaxis()
     plt.show()
 
-def main():
-    view_points()
+def read_cam_calibration(filename):
+    with open(filename,'r') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
 
+    K = np.array(data['camera_matrix']['data']).reshape(3,3)
+    R = np.array(data['R_cam_world']).reshape(3,3)
+    t = np.array(data['t_world_cam'])
+    cam_param = CameraParam(K, R, -R@t)
+
+    #  ------------ test,  pose in gtsam should be camera pose in the world frame ------------
+    # K_gtsam = gtsam.Cal3_S2(K[0,0], K[1,1], K[2,2], K[0,2], K[1,2])
+    # R_gtsam = gtsam.Rot3(R.T) 
+    # t_gtsam = gtsam.Point3(t[0],t[1],t[2])
+    # pose1 = gtsam.Pose3(R_gtsam, t_gtsam)
+    # camera1_gtsam = gtsam.PinholeCameraCal3_S2(pose1, K_gtsam)
+
+    # p0 =  np.array([0,0,0])
+    # print(camera1_gtsam.project(p0))
+    # print(cam_param.proj2img(p0))
+
+    return cam_param
+
+
+def view_triangulation():
+    detections = {f'camera_{i}': [] for i in range(1,7)}
+    # bag = rosbag.Bag('/home/qingyu/Downloads/20241209_tennis/data1_2024-12-09-17-32-32.bag')
+    camera_names = ['22495525','22495526','22495527','23045007','23045008','23045009']
+    date = 'Jul18'
+    cam_params = [read_cam_calibration(f'conf/camera/{cname}_calibration_{date}.yaml') for cname in camera_names]
+    cam_params_dict =  {'camera_'+str(i+1):cam_params[i] for i in range(6)}
+
+    print(cam_params_dict)
+    raise
+    # read all detections
+    for topic, msg, t in bag.read_messages():
+        camera_id = topic.split('/')[1]
+        for p in msg.points:
+            detections[camera_id].append([msg.header.stamp.to_sec(), p.x, p.y])
+    bag.close()
+
+    #
+def main():
+    # view_points()
+    view_triangulation()
 
 
 
